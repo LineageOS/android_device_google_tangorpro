422f502243d68919fa84d57f28b609f5f1e0866c
Nolen Johnson <johnsonnolen@gmail.com>
2023-06-29 17:16:06 -0400

    tangorpro: Conditionally enable GMS-specific product overlays
    Change-Id: I8eafc030e83dcd807191ea5941bf11e493617683

--- device-tangorpro.mk
+++ device-tangorpro.mk
@@ -267,4 +267,5 @@ PRODUCT_COPY_FILES += \
 # SKU specific RROs
 PRODUCT_PACKAGES += \
     GrilRadioCustomizableNoRadio \
-    SettingsOverlayGTU8P
+    SettingsOverlayGTU8P \
+    TangorproGMSOverlay
